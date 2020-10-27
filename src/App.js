82ce26d4378666929c3c529d97ab167ed48d0359
khalidwilliams <khalidwill27@gmail.com>
2020-10-27 08:46:01 -0600

    Make gif search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      gifs: [],
+      searchQuery: '',
+      error: ''
+    }
+    this.apiKey = 'crVOwUQBO7BRdnKWyO3F1jCeNxZWjshH';
+  }
+
+  updateSearch = (event) => {
+    this.setState({searchQuery: event.target.value});
+  }
+
+  searchForGifs = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(`http://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${this.state.searchQuery}&rating=g`);
+      const gifData = await response.json();
+      console.log(gifData);
+      this.setState({gifs: gifData.data, searchQuery: ''})
+    } catch (error) {
+      this.setState({error: error});
+    }
+      
+  }
+  render() {
+    const allGifs = this.state.gifs
+      .map(gif => <img key={gif.id} src={gif.url} alt={gif.title} />)
+
+    return (
+      <div className="App">
+        <h1>Hey search something</h1>
+        <input type="text" value={this.state.searchQuery} onChange={this.updateSearch} />
+        <button onClick={this.searchForGifs}>Search</button>
+        {this.state.error}
+        <section>{allGifs}</section>
+      </div>
+    );
+  }
 }
 
 export default App;
